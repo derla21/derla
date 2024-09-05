ae8567bf382abffc92562ce9a2e8b33f1421fbb8
STEVEN G. DERLA <23010867@csabcomlab.edu.ph>
2024-09-05 18:35:52 +0800

    Adding index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php  
+
+class Payroll {
+
+	private string $firstname = "Steven";
+	private string $lastname = "Derla";
+
+	private int $tax = 50;
+	private int $salary = 100;
+
+	public function computeNet() : float {
+		$net = $this->salary - $this->tax; 
+		return $net;
+	}
+
+	public function getEmployeeName() : string {
+		return $this->firstname.", ".$this->lastname; 
+
+	}
+
+
+}
+
+$Payroll = new Payroll();
+
+echo "Employee Name: ". $Payroll->getEmployeeName() . "<br/>";
+echo "Net Pay : ". $Payroll->computeNet();
+
+?>
